e5d67c93dcde694d88ec30291d493ba08892eed7
Justin-Desktop <justin.g.shay@gmail.com>
2024-10-07 01:33:17 -0400

    work in progress

--- bubble.js
+++ bubble.js
@@ -1,3 +1,23 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+    //if array empty or 1 index long return immediately
+    if(arr.length < 2) return arr;
+    //runs arr.length times
+    for(let i = 0; i < arr.length; i++) {
+        let swapped = false;
+        for(let j = 0; j < arr.length; j++) {
+            console.log(arr);
+            if(arr[j] > arr[j+1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                swapped = true;
+            }
+        }
+        if(!swapped) break;
+    }
+    return arr;
+}
 
 module.exports = bubbleSort;
+
+//bubbleSort([5,8,9,-3,4]);
--- merge.js
+++ merge.js
@@ -1,5 +1,42 @@
-function merge() {}
+function merge(arr1, arr2) {
+    console.log("Merging " + arr1 + " and " + arr2);
+    //empty to merge sorted arrays
+    const resultsArr = [];
+    let i = 0, j = 0;
+    //compare and push smaller value until 1 array exhausted
+    while(i < arr1.length && j < arr2.length) {
+        if(arr1[i] < arr2[j]) {
+            resultsArr.push(arr1[i]);
+            i++;
+        } else {
+            resultsArr.push(arr2[j]);
+            j++;
+        }
+    }
+    //merge non-exhausted array to results
+    while(i < arr1.length) {
+        resultsArr.push(arr1[i]);
+        i++;
+    }
+    while(j < arr2.length) {
+        resultsArr.push(arr2[j]);
+        j++;
+    }
+    console.log("Merged array: ", resultsArr);
+    return resultsArr;
+}
 
-function mergeSort() {}
+function mergeSort(arr) {
+    if(arr.length < 2) return arr;
+    const midPoint = Math.floor(arr.length / 2);
+    //recursively break down arrays to smaller size to order
+    const leftHalf = mergeSort(arr.slice(0, midPoint));
+    const rightHalf = mergeSort(arr.slice(midPoint));
+
+    return merge(leftHalf, rightHalf);
+}
 
 module.exports = { merge, mergeSort};
+
+//merge([1,3,5,7],[2,4,6,7,8]);
+mergeSort([99,1,15,-18,7,29,3,83,0,-64,55]);
